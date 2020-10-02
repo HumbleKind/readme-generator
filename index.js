a6e00d828846c8ade154e21c4ecfaae5715f52db
Christian Sinai <christian_sinai@me.com>
2020-10-02 09:44:34 -0700

    removed in-line code 'comments'

--- index.js
+++ index.js
@@ -1,38 +1,8 @@
-// use the built-in 'require' function to include the Node.js File System module (to modify local files)
 const fs = require("fs");
-
-// include the Inquirer.js command line user interface module for Node.js
 const inquirer = require("inquirer");
-
-// QUESTION: why do I need this module ...
 const util = require("util");
 
 const writeFileAsync = util.promisify(fs.writeFile);
-
-// initial test code to check functionality
-// inquirer.prompt([
-//     {
-//         type: "input",
-//         message: "Please enter the title for your project (i.e. README Generator):",
-//         name: "title"
-//     }
-// ]).then(function(response) {
-//     fs.writeFile("README.md", response.title, function(err) {
-//         if (err) {
-//             return console.log(err);
-//         }
-//         console.log("Success!");
-// 
-        // provide command line feedback to check if writeFile function is working
-        // fs.readFile("README.md", "utf8", function(error, data) {
-        //     console.log("READING README.MD ...");
-        //     if (error) {
-        //         return console.log(error);
-        //     }
-        //     console.log(data);
-        // })
-//     })
-// })
 
 function promptUser() {
     return inquirer.prompt([
@@ -91,25 +61,11 @@ function promptUser() {
 
 function generateMD(answers) {
 
-        // the following needed modification to function as intended
-        // const licenseMIT = "A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.";
-        // const licenseGPL = "Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.";
-        // const licenseApa = "A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.";
-
-        // function generateLicense(license) {
-        //     if (license == "Apache--2.0") {
-        //         let licenseType = "Apache 2.0: " + licenseApa;
-        //         return licenseType;
-        //     }
-        // }
-
-        // generateLicense(answers.license);
-    
     // declare and initialize the 'licenseType' variable used globally/ below (in "License section")
     let licenseType = "";
     
     function generateLicense(license) {
-        // create each individual license summary, inside license generating function (the only place these variable are used)
+        // create each individual license summary, inside this license generating function (i.e. the only place these variables are used)
         const licenseMIT = "A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.";
         const licenseGPL = "Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.";
         const licenseApache = "A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.";
